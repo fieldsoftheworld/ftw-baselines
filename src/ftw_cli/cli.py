8e63e01bac104d5cfb5065c24e423ea390d4d77e
Chris Holmes <cholmes@planet.com>
2024-10-21 19:54:55 -0700

    refactoring CLI as proposed in #21

--- src/ftw_cli/cli.py
+++ src/ftw_cli/cli.py
@@ -2,15 +2,23 @@ import click
 from ftw_cli.download import download
 from ftw_cli.unpack import unpack
 from ftw_cli.model import model
+from ftw_cli.inference import inference
 
 @click.group()
 def ftw():
     """Fields of The World (FTW) - Command Line Interface"""
     pass
 
-ftw.add_command(download, "download")
-ftw.add_command(unpack, "unpack")
+@ftw.group()
+def data():
+    """Downloading, unpacking, and preparing the FTW dataset."""
+    pass
+
+data.add_command(download, "download")
+data.add_command(unpack, "unpack")
+
 ftw.add_command(model, "model")
+ftw.add_command(inference, "inference")
 
 if __name__ == "__main__":
     ftw()
