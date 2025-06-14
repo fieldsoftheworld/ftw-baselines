4a39e3cf757604dd44b09c04dc3ef3bcbe217a05
Matthias Mohr <webmaster@mamo-net.de>
2025-06-14 14:19:58 +0200

    Make inference more robust against different Sentinel-2 processing versions #86 (#87)

--- src/ftw_cli/download_img.py
+++ src/ftw_cli/download_img.py
@@ -41,24 +41,39 @@ def create_input(win_a, win_b, out, overwrite, bbox = None):
         bbox = list(map(float, bbox.split(",")))
 
     # Load the items
-    item_win_a = get_item(win_a)
-    item_win_b = get_item(win_b)
+    identifiers = [win_a, win_b]
+    items = []
+    timestamp = None
+    version = 0
+    for i in identifiers:
+        item = get_item(i)
+        items.append(item)
 
+        datetime = item.datetime or item.start_datetime or item.end_datetime
+        if datetime and (not timestamp or datetime > timestamp):
+            timestamp = datetime
+
+        proc_version = item.properties.get("processing:version", item.properties.get("s2:processing_baseline", 0))
+        try:
+            proc_version = float(proc_version)
+        except TypeError:
+            proc_version = 0
+        if proc_version > 0:
+            if version > 0 and version != proc_version:
+                print('Processing version of imagery differs. Exiting.')
+                return
+            version = proc_version
+
+    shapes = [shape(item.geometry) for item in items]
     # Ensure the images intersect
-    geometry1 = shape(item_win_a.geometry)
-    geometry2 = shape(item_win_b.geometry)
-    if not geometry1.intersects(geometry2):
+    if not shapes[0].intersects(shapes[1]):
         print("The provided images do not intersect. Exiting.")
         return
-
-    # Get the latest timestamp
-    timestamps = list(filter(lambda x: x is not None, [item_win_a.datetime, item_win_b.datetime]))
-    timestamp = max(timestamps) if len(timestamps) > 0 else None
 
     print("Loading data")
     tic = time.time()
     data = odc.stac.load(
-        [item_win_a, item_win_b],
+        [items[0], items[1]],
         bands=BANDS_OF_INTEREST,
         dtype="uint16",
         resampling="bilinear",
@@ -68,6 +83,12 @@ def create_input(win_a, win_b, out, overwrite, bbox = None):
 
     print("Merging data")
     data = data.to_array(dim="band").stack(bands=("time", "band")).drop_vars("band").transpose('bands', 'y', 'x')
+
+    if version < 3 or version >= 4:
+        print(f"Processing version {version} unknown or untested (< 3.0 or >= 6.0). Inference quality might decrease.")
+    if version >= 4:
+        print(f"Rescaling data to processing version 3.0 from processing version {version}.")
+        data = (data.astype("int32") - 1000).clip(min=0).astype("uint16")
 
     print("Writing output")
     data.rio.to_raster(
