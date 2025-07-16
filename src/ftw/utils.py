d2139dfdf2f9917d22c99262faf43cf73d5ce70a
Martha Morrissey <marthamorrissey93@gmail.com>
2025-07-16 10:41:35 -0600

    circ mean + additional test

--- src/ftw/utils.py
+++ src/ftw/utils.py
@@ -1,13 +1,18 @@
 import hashlib
+import logging
 import os
 
 import numpy as np
 import pandas as pd
+import scipy
+import scipy.stats
 import xarray as xr
 
 # Harvest day raster paths from https://github.com/ucg-uv/research_products/tree/main
 SUMMER_START_RASTER_PATH = "assets/global_crop_calendar/sc_sos_3x3_v2_cog.tiff"
 SUMMER_END_RASTER_PATH = "assets/global_crop_calendar/sc_eos_3x3_v2_cog.tiff"
+
+logger = logging.getLogger()
 
 
 def compute_md5(file_path):
@@ -85,7 +90,11 @@ def get_harvest_integer_from_bbox(
     )
 
     if len(start_value["band_data"][0][0]) > 1:
-        start_value = int(np.mean(start_value["band_data"][0][0]))
+        start_days = start_value["band_data"].values[0][0]
+        logger.info(
+            f"Multiple dates found in area of interest {start_days}. Using circular mean to determine harvest day."
+        )
+        start_value = int(round(scipy.stats.circmean(start_days, high=365, low=1)))
     else:
         start_value = int(start_value["band_data"].values[0][0][0])
 
@@ -93,7 +102,11 @@ def get_harvest_integer_from_bbox(
         bbox[0], bbox[1], bbox[2], bbox[3], allow_one_dimensional_raster=True
     )
     if len(end_value["band_data"][0][0]) > 1:
-        end_value = int(np.mean(end_value["band_data"][0][0]))
+        end_days = end_value["band_data"].values[0][0]
+        logger.info(
+            f"Multiple dates found in area of interest {end_days}. Using circular mean to determine harvest day."
+        )
+        end_value = int(round(scipy.stats.circmean(end_days, high=365, low=1)))
 
     else:
         end_value = int(end_value["band_data"].values[0][0][0])
