57a4ddd3cf635cb1abddda14179203d35c418d92
Martha Morrissey <marthamorrissey93@gmail.com>
2025-08-13 10:26:23 -0600

    oops use new use_mcp argument

--- tests/test_download_img.py
+++ tests/test_download_img.py
@@ -7,7 +7,7 @@ from ftw_tools.download.download_img import get_item, query_stac
 
 def test_get_item_from_s3_url():
     s3_url = "s3://sentinel-cogs/sentinel-s2-l2a-cogs/15/T/VG/2021/12/S2A_15TVG_20211225_0_L2A"
-    item = get_item(s3_url)
+    item = get_item(s3_url, use_mcp=False)
     assert type(item) is pystac.Item
 
 
