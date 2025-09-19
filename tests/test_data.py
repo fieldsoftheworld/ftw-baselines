6e051b42296b8d3f9eefebee0b8ea820b3ef4c12
Andrew Smith <159601423+asmithml@users.noreply.github.com>
2025-09-19 15:03:04 -0600

    feat: new pixi build task for publishing to pypi (#157)
    * bld: moved deps to pixi.toml
    
    * feat: ability to build pypi from pixi.toml
    
    * Fixes for WSL and Windows (#154)
    
    * Fix delineate anything on WSL
    
    * Move rasterio from pypi to conda dependencies to install GDAL properly on Windows
    
    * Print more details in tests
    
    * Fix link to issue
    
    * Require later version of ultralytics, older versions may fail to due pands -> polars change in ultralytics
    
    * Add ftw to dependencies
    
    * Make tests work on Windows
    
    * chg: updated build script to align w/ windows fix
    
    * fix: linting issues
    
    * ci: workflow must happen within env with delineate anything
    
    ---------
    
    Co-authored-by: Matthias Mohr <webmaster@mamo-net.de>

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,7 +11,9 @@ def test_data_download():
 
     # Clean download
     result = runner.invoke(download, ["-f", "--countries=Rwanda", "--no-unpack"])
-    assert result.exit_code == 0, result.output
+    assert result.exit_code == 0, (
+        f"Exited with {result.exit_code}. Output: {result.stdout} {result.stderr}"
+    )
     assert "Downloading selected countries: ['rwanda']" in result.output
     assert "Overall Download Progress: 100%" in result.output
     assert "Unpacking files:" not in result.output
@@ -20,7 +22,9 @@ def test_data_download():
 
     # Try again and expect to skip the download, but unpack files
     result = runner.invoke(download, ["--countries=Rwanda"])
-    assert result.exit_code == 0, result.output
+    assert result.exit_code == 0, (
+        f"Exited with {result.exit_code}. Output: {result.stdout} {result.stderr}"
+    )
     assert "already exists, skipping download." in result.output
     assert "Unpacking files: 100%" in result.output
     assert os.path.exists("data/rwanda.zip")
@@ -28,7 +32,9 @@ def test_data_download():
 
     # Try again and expect to skip the download and not unpack
     result = runner.invoke(download, ["--countries=Rwanda"])
-    assert result.exit_code == 0, result.output
+    assert result.exit_code == 0, (
+        f"Exited with {result.exit_code}. Output: {result.stdout} {result.stderr}"
+    )
     assert "already exists, skipping download." in result.output
     assert "Unpacking files:" in result.output
 
@@ -38,7 +44,9 @@ def test_data_unpack():
 
     # Unpack the files
     result = runner.invoke(unpack, [])
-    assert result.exit_code == 0, result.output
+    assert result.exit_code == 0, (
+        f"Exited with {result.exit_code}. Output: {result.stdout} {result.stderr}"
+    )
     assert "Unpacking files: 100%" in result.output
 
     # Error with non-existing folder
